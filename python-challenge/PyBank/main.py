4baf11602ef6b73a76fd547d8191753699dac77b
efarrell3 <69441481+efarrell3@users.noreply.github.com>
2020-09-29 22:38:39 -0400

    Add files via upload

--- python-challenge/PyBank/main.py
+++ python-challenge/PyBank/main.py
@@ -2,39 +2,52 @@
 import os
 import csv
 
-bank_csv = os.path.join("..", "Resources", "budget_data.csv")
+bank_csv = os.path.join("Resources", "budget_data.csv")
 
 #Specify the file to write to
-outPath = os.path.join("..", "analysis", "bank.csv")
+outPath = os.path.join("analysis", "bank.csv")
 
 #Open the file using "write" mode.  Specify the variable to hold the content
-with open(bank_csv) as csvfile:
-    csvreader=csv.reader(csvfile, delimeter=",")
-    for row in csvreader:
-        #Find the total number of months
+with open(bank_csv, 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    csvheader = next(csvreader, None)
+    #Find the number of months.  Subtracting the first row because it's the header
+    months = len(list(csvreader))
+
+    #Sum all of the data in the second column
+    money = int(csvreader[1])
+    total = sum(money)
+
+    #Find the greatest increase in profits
+    #dateinc will be the date that the greatest increase in profits occurs
+    #amountinc variable will represent the amount of the greatest increase in profits
+
+    #Find the greatest decrease in profits
+    #datedec will be the date that the greatest decrease in profits occurs
+    #amountdec variable will represent the amount of the greatest decrease in profits
+
+
 
     
 with open(outPath, 'w') as datafile:
     
     #initialize the csv.writer
-    csvwriter = csv.writer(datafile, delimeter=',')
+    csvwriter = csv.writer(datafile, delimiter=",")
 
     #write the first row (column headers)
-    csvwrite.writerow(["Date", "Profit/Losses"])
+    csvwriter.writerow(["Financial Analysis"])
 
     #write the second row
-    
+    csvwriter.writerow(["Total Months:", months])
 
+    # write the third row printing the sum of profits and losses in the second column
+    csvwriter.writerow(["Total:", total])
 
-#Create variables for total months and calculate the total months from the csv file
-TotMonths
+    #write the fourth row
+    csvwriter.writerow(["Greatest Increase in Profits:", dateinc, amountinc])
 
+     #write the fifth row
+    csvwriter.writerow(["Greatest Decrease in Profits:", datedec, amountdec])
 
-
-
-
-
-# Print a statement displaying the total months
-print(f"Total Months: + {TotMonths}")
 
 
--- python-challenge/PyPoll/main.py
+++ python-challenge/PyPoll/main.py
@@ -2,3 +2,38 @@
 import os
 import csv
 
+poll_csv = os.path.join("Resources", "election_data.csv")
+
+#Specify the file to write to
+outPath = os.path.join("analysis", "poll.csv")
+
+#Open the file using "write" mode.  Specify the variable to hold the content
+with open(poll_csv, 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    csvheader = next(csvreader, None)
+    #Find the number of months.  Subtracting the first row because it's the header
+    votes = len(list(csvreader))
+
+    #
+
+
+
+
+
+
+    
+with open(outPath, 'w') as datafile:
+    
+    #initialize the csv.writer
+    csvwriter = csv.writer(datafile, delimiter=",")
+
+    #write the first row (column headers)
+    csvwriter.writerow(["Election Results"])
+
+    #write the second row
+    csvwriter.writerow(["Total Votes:", votes])
+
+
+
+#Create variables for total months and calculate the total months from the csv file
+# vTotMonths
